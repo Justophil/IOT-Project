31e990b07f251bb442fc61e20ddbc6a0c067c334
phil xyz <89649491+Justophil@users.noreply.github.com>
2023-09-28 17:50:36 -0400

    fixed the image switching on the dashboard

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ IMG_ON = '/assets/img/on.png'
 IMG_OFF = '/assets/img/off.png'
 # GPIO.setwarnings(False) # Ignore warning for now
 # GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-LED=17
+# LED=17
 # GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
 
 app.layout = html.Div([
@@ -20,17 +20,23 @@ app.layout = html.Div([
         ]),
 ])
 @callback(
-    [Output('bulb', 'src')],
+    #adding an array means the input or output must contain an array
+    Output('bulb', 'src'),
     Input('button-on-and-off', 'n_clicks')
 )
 def update_LED (n_clicks):
     click = n_clicks % 2
     if click:
         # GPIO.output(LED, GPIO.HIGH)
-        return IMG_ON 
+        return IMG_ON
     else:
         # GPIO.output(LED, GPIO.LOW)
         return IMG_OFF
 
 if __name__ == '__main__':
+    # this is a theory but
+    # if you write your ip from another device and port X.X.X.X:8050 on a browser 
+    # you can connect to the website from that device
+    # app.run(host='0.0.0.0',debug=True)
+    # this is if you want to run it on your current device
     app.run(debug=True)
